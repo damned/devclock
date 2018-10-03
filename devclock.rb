ee39682124ac7d52202d7f02f8c2b51035157c8f
damned <writetodan@yahoo.com>
2018-10-03 17:30:19 +0100

    @damned test using system to make copy of file on each detected change
    seems pretty cool starting point, can do offline/quiet time processing
    if want to consolidate, but these copies are small cos source files small.
    
    and easy to do in ruby without it sweating

--- devclock.rb
+++ devclock.rb
@@ -1,7 +1,15 @@
 require 'listen'
+require 'fileutils'
+
+def snapshots_dir
+  './snapshots/'
+end
+
+FileUtils.mkdir_p snapshots_dir
 
 def changed(change, f)
   puts "#{Time.now},#{change},#{f}"
+  puts `time cp #{f} "#{snapshots_dir}#{File.basename(f)}.#{Time.now.to_i}"`
 end
 
 listener = Listen.to(*ARGV.to_a) do |modified, added, removed|
